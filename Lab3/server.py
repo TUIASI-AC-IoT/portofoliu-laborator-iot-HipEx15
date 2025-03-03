a02ee884123a554daee5bcc1bde91da5e20e395e
Alex <86966930+HipEx15@users.noreply.github.com>
2025-03-03 19:44:17 +0200

    Lab3

---
+++ Lab3/server.py
@@ -0,0 +1,27 @@
+import io
+from flask import Flask, send_file
+import os.path
+
+app = Flask(__name__)
+
+@app.route('/firmware.bin')
+def firm():
+    with open(".pio\\build\\esp-wrover-kit\\firmware.bin", 'rb') as bites:
+        print(bites)
+        return send_file(
+                     io.BytesIO(bites.read()),
+                     mimetype='application/octet-stream'
+               )
+@app.route('/version')
+def version():
+    with open("versioning", 'r') as f:
+        version_content = f.read()
+        print(version_content)
+    return version_content
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', ssl_context=('ca_cert.pem', 'ca_key.pem'), debug=True)
